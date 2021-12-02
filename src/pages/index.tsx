874f1b1da318bfaf866db2950bf58655cbed1b5e
Ricardo Augusto <ricardoaugusto.eb@gmail.com>
2021-12-02 14:35:58 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
-import { useEffect } from "react"
-
-export default function Home() {
-  useEffect(() => {
-    fetch('http://localhost:3333/episodes')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
-  
+export default function Home() { 
   return (
     <h1>Index</h1>
   )
+}
+
+export async function getServerSideProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    }
+  } 
+  
 }
