83221d17d6106f7b5c70c4f62d402592215e6bf8
ricardoaugusto1210 <ricardoaugusto.eb@gmail.com>
2021-12-02 12:11:16 -0300

    firts commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react"
+
+export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+  
+  return (
+    <h1>Index</h1>
+  )
+}
